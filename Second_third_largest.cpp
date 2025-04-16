fa5a3d9f91e7a580870d10039a7b0aa516a010a0
Gech0463 <gechgech0463@gmail.com>
2025-04-16 22:07:35 +0300

    This is the code to impliments the second and third largest number

--- Second_third_largest.cpp
+++ Second_third_largest.cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+void findSecondAndThirdLargest(int arr[], int size, int &secondLargest, int &thirdLargest) {
+    int firstLargest = INT_MIN;
+    secondLargest = INT_MIN;
+    thirdLargest = INT_MIN;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > firstLargest) {
+            thirdLargest = secondLargest;
+            secondLargest = firstLargest;
+            firstLargest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != firstLargest) {
+            thirdLargest = secondLargest;
+            secondLargest = arr[i];
+        } else if (arr[i] > thirdLargest && arr[i] != secondLargest && arr[i] != firstLargest) {
+            thirdLargest = arr[i];
+        }
+    }
+}
+
+int main() {
+    int arr[] = {120, 45, 67, 89, 34, 23, 90, 11};
+    int size = sizeof(arr) / sizeof(arr[0]); // Calculate array size
+
+    int secondLargest, thirdLargest;
+    findSecondAndThirdLargest(arr, size, secondLargest, thirdLargest);
+
+    cout << "The second largest number in the array is: " << secondLargest << endl;
+    cout << "The third largest number in the array is: " << thirdLargest << endl;
+
+    return 0;
+}
